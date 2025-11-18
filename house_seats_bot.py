e6c248469fc8700d2f027c6fe18a86de1eaf6a16
Ethanwang17 <ejw17@uw.edu>
2025-11-17 18:49:54 -0800

    updated hours for testing

--- house_seats_bot.py
+++ house_seats_bot.py
@@ -272,7 +272,7 @@ async def scraping_task():
 	current_time = datetime.now(PST_TIMEZONE)
 	
 	# Check if current time is between 6 AM and 5 PM PST
-	if 6 <= current_time.hour < 17:
+	if 6 <= current_time.hour < 20:
 		await asyncio.to_thread(scrape_and_process)
 	else:
 		logger.debug("Outside of operating hours (8 AM - 5 PM PST). Skipping scrape.")
